1a5cfbb2ed32d2e6b004134b3d3cf9c0a5bf058c
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-11-26 13:26:28 +0700

    Refactor health setter to remove return values; enhance Player class with attack, defend, collect_item, and view_item methods

--- models/creatures.py
+++ models/creatures.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from items import Inventory
+import config
 
 class Creature(ABC):
     def __init__(self, name, max_hp, damage):
@@ -24,11 +26,37 @@ class Creature(ABC):
         if new_hp:
             if new_hp >= 0 and new_hp <= self.max_hp:
                 self.__hp = new_hp
-                return True
             else:
-                return False
+                print("Invalid HP value")
         else:
-            return False
+            print("Invalid HP value")
 
     def __str__(self):
         return f"{self.name.upper()}\nHP\t: ({self.health}/{self.max_hp})\nDamage\t: {self.damage}"
+
+class Player(Creature):
+    def __init__(self, name):
+        super().__init__(name, config.PLAYER["MAX_HP"], config.PLAYER["BASE_DAMAGE"])
+        self.inventory = Inventory()
+
+    def attack(self, target):
+        print(f"{self.name} attacks {target} with {self.damage} damage")
+        target.defend(self.damage)
+
+    def defend(self, damage):
+         if damage >= 0:
+            new_hp = self.health - damage
+
+            if new_hp < 0:
+                self.health = new_hp
+                print(f"{self.name} is DIE")
+            else:
+                print(f"{self.name} takes {damage} damage\nHP\t: ({self.health}/{self.max_hp})")
+
+    def collect_item(self, item):
+        self.inventory.add_item(item)
+        print("Item added to Inventory")
+    
+    def view_item(self):
+        self.inventory.list_item()
+
