25b901b8ead7c5524a1462b68fbf0865ed8d71a5
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-11-26 11:18:27 +0700

    Add configuration for player and monsters data

---
+++ config.py
@@ -0,0 +1,33 @@
+# Konstanta umum
+SAVE_FILE_NAME = "babad_save.json"
+INITIAL_ROOM_KEY = "cloud"
+
+# Data Player
+PLAYER = {
+    "MAX_HP"   : 5000,
+    "BASE_DAMAGE"   : 100
+}
+
+# Data Monsters
+MONSTERS = {
+    "DRAGON": {
+        "NAME" : "Dragon",
+        "HP"   : 9999,
+        "DAMAGE": 999
+    },
+    "GOBLIN": {
+        "NAME" : "Goblin",
+        "HP"   : 200,
+        "DAMAGE": 50
+    },
+    "SKELETON" : {
+        "NAME" : "Skeleton",
+        "HP"   : 350,
+        "DAMAGE": 75
+    },
+    "MINOTAUR" : {
+        "NAME" : "Minotaur",
+        "HP"   : 500,
+        "DAMAGE": 300
+    }
+}
