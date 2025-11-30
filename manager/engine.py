0920b03dcfe95b98d4e196202a5a87bd4712512f
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-11-30 14:49:48 +0700

    Refactor GameEngine and ConsoleView for improved game initialization and loading screen display

--- manager/engine.py
+++ manager/engine.py
@@ -1,13 +1,15 @@
-from view.view import init
+from view.view import ConsoleView
 from models.world import World
 
 class GameEngine:
     def __init__(self):
+        self.view = ConsoleView()
         self.world = World()
 
     def start_game(self):
         self.world.generate_world()
 
+        self.view.show_loading_screen()
 
             
             
--- view/view.py
+++ view/view.py
@@ -1,17 +1,43 @@
 from models import creatures
 import os
+import time
+import shutil
+import sys
 
-def status(creature):
-    print(creature)
+class ConsoleView:
+    def __init__(self):
+        self.width_console = shutil.get_terminal_size().columns
+        
+    def clear_screen(self):
+        os.system("cls" if os.name == "nt" else "clear")
 
-def init():
-    print(f"{'-' * 100}\n{' ROLE PLAYING GAME ':=^100}\n{' by Fizz ':-^100}")
-    karakter = int(input(
-        f"\nKarakter yang tersedia:" +
-        f"\n1. Fighter" +
-        f"\n2. Archer" +
-        f"\n3. Tank" +
-        f"\nSilahkan pilih berdasarkan angka (1-3) : "
-        ))
+    def padding_center(self, text):
+        padding = (self.width_console - len(text)) / 2
+        return int(padding)
+
+    def typing_slow(self, text, speed=0.05, center=True):
+        if center:
+            sys.stdout.write(" " * self.padding_center(text))
+
+        for char in text:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(speed)
+        print()
     
-    return karakter
+    def show_loading_screen(self):
+        self.clear_screen()
+
+        print('\n'*16)
+        self.typing_slow("W E L C O M E   T O   M U R I M   W O R L D")
+        print()
+        self.typing_slow('by   F I Z Z')
+
+        time.sleep(0.75)
+
+        print("\n"*4)
+
+        text_enter = 'Press [ENTER] to Start....'
+        input(' ' * self.padding_center(text_enter) + text_enter)
+        self.clear_screen()
+
