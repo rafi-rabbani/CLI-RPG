b989a8c5dae75a99e2268bfb4b22551623e5bad0
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-11-30 16:58:30 +0700

    Refactor PLAYER and MONSTERS data structure for improved readability and consistency

--- config.py
+++ config.py
@@ -4,40 +4,40 @@ INITIAL_ROOM_KEY = "cloud"
 
 # Data Player
 PLAYER = {
-    "FIGHTER" : {
-        "HP" : 200,
-        "DAMAGE" : 60
+    "FIGHTER"   : {
+        "HP"    :    200,
+        "DAMAGE": 60
     },
-    "ARCHER" : {
-        "HP" : 120,
-        "DAMAGE" : 100
+    "ARCHER"    : {
+        "HP"    : 120,
+        "DAMAGE": 100
     },
-    "TANK" : {
-        "HP" : 500,
-        "DAMAGE" : 24
+    "TANK"      : {
+        "HP"    : 500,
+        "DAMAGE": 24
     }
 }
 
 # Data Monsters
 MONSTERS = {
-    "GOBLIN": {
-        "NAME" : "Goblin",
-        "HP"   : 200,
+    "GOBLIN"    : {
+        "NAME"  : "Goblin",
+        "HP"    : 200,
         "DAMAGE": 50
     },
-    "SKELETON" : {
-        "NAME" : "Skeleton",
-        "HP"   : 350,
+    "SKELETON"  : {
+        "NAME"  : "Skeleton",
+        "HP"    : 350,
         "DAMAGE": 75
     },
-    "MINOTAUR" : {
-        "NAME" : "Minotaur",
-        "HP"   : 500,
+    "MINOTAUR"  : {
+        "NAME"  : "Minotaur",
+        "HP"    : 500,
         "DAMAGE": 300
     },
-    "DRAGON": {
-        "NAME" : "Dragon",
-        "HP"   : 9999,
+    "DRAGON"    : {
+        "NAME"  : "Dragon",
+        "HP"    : 9999,
         "DAMAGE": 999
     }
 }
