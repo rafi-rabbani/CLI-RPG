df24e95107b7735872ff02df389073332bcb0044
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-11-30 12:13:16 +0700

    Refactor World class to encapsulate room generation and connections for improved structure

--- models/world.py
+++ models/world.py
@@ -9,21 +9,35 @@ class Room:
     def connects(self, direction, next_room):
         self.exits[direction] = next_room
 
-cloud = Room("Cloud Association", "Association of Players from All Over of The World")
-orthodox = Room("Orthodox Alliance", "The Central Headquarters of Murim Orthodox")
-unorthodox = Room("Unorthodox Alliance", "The Alliance of Unorthodox Murim Martial Artist")
-demonic = Room("Heavenly Demonic Cult", "A Cult Founded by Cheon Ma, The First Heavenly Demon Emperor")
-ice = Room("North Sea Ice Palace", "A palace located in the North Sea whose main attribute is ice")
+class World:
+    def __init__(self):
+        self.rooms = {}
+        self.starting_room = None
+    
+    def generate_world(self):
+        cloud = Room("Cloud Association", "Association of Players from All Over of The World")
+        orthodox = Room("Orthodox Alliance", "The Central Headquarters of Murim Orthodox")
+        unorthodox = Room("Unorthodox Alliance", "The Alliance of Unorthodox Murim Martial Artist")
+        demonic = Room("Heavenly Demonic Cult", "A Cult Founded by Cheon Ma, The First Heavenly Demon Emperor")
+        ice = Room("North Sea Ice Palace", "A palace located in the North Sea whose main attribute is ice")
 
-cloud.connects("west", orthodox)
+        orthodox.connects("north", ice)
+        orthodox.connects("south", demonic)
+        orthodox.connects("west", cloud)
+        orthodox.connects("east", unorthodox)
 
-orthodox.connects("north", ice)
-orthodox.connects("south", demonic)
-orthodox.connects("west", cloud)
-orthodox.connects("east", unorthodox)
+        cloud.connects("west", orthodox)
+        unorthodox.connects("east", orthodox)
+        demonic.connects("north", orthodox)
+        ice.connects("south", orthodox)
 
-unorthodox.connects("east", orthodox)
+        self.rooms = {
+            "center" : orthodox,
+            "north" : ice,
+            "south" : demonic,
+            "west" : unorthodox,
+            "east" : cloud
+        }
 
-demonic.connects("north", orthodox)
+        self.starting_room = cloud
 
-ice.connects("south", orthodox)
