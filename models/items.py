099863d7bcd090929576dadee133d8b1f6c005ff
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-12-07 21:46:49 +0700

    style(items): apply black formatter
    Reformatted Item and Inventory classes to comply with Black standards (expanded one-liners to multi-line blocks).

--- models/items.py
+++ models/items.py
@@ -3,9 +3,12 @@ class Item:
         self.name = name
         self.description = description
 
-    def __str__(self) : return f"Name\t\t: {(self.name)}\nDescription\t: {self.description}\n"
-    
-    def __repr__(self): return f"{self.name}"
+    def __str__(self):
+        return f"Name\t\t: {(self.name)}\nDescription\t: {self.description}\n"
+
+    def __repr__(self):
+        return f"{self.name}"
+
 
 class Inventory:
     def __init__(self):
@@ -17,4 +20,5 @@ class Inventory:
         else:
             return False
 
-    def list_item(self): return self.__item_list
+    def list_item(self):
+        return self.__item_list
